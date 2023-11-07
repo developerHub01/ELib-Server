0da9b7dd2450aaa86f825fbc2382dbf777bf44b5
developerHub01 <developerhubytchannel@gmail.com>
2023-11-07 17:54:19 +0600

    book get and book get by category added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const run = async () => {
     await client.connect();
 
     const booksCollection = client.db("eLib").collection("books");
+    const borrowedBooksCollection = client.db("eLib").collection("borrowed");
 
     app.get("/", async (req, res) => {
       res.send("Home route..............");
@@ -36,13 +37,77 @@ const run = async () => {
       res.send(result.acknowledged);
     });
 
+    app.post("/borrowed", async (req, res) => {
+      const borrowData = req.body;
+      const { email, id } = borrowData;
+      borrowData.borrowedDate = new Date().toISOString().split("T")[0];
+      let result = await borrowedBooksCollection.findOne({
+        email,
+        id,
+      });
+
+      if (result) return res.send({ message: "Already in borrow" });
+
+      const currentQuantity = await booksCollection.findOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          projection: {
+            _id: 0,
+            quantity: 1,
+          },
+        }
+      );
+
+      if (!currentQuantity.quantity) {
+        return res.send({ message: "Not available" });
+      }
+
+      await booksCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $inc: {
+            quantity: -1,
+          },
+        }
+      );
+
+      result = await borrowedBooksCollection.insertOne(borrowData);
+
+      return res.send({ message: result.acknowledged });
+    });
+
+    app.get("/borrowed/:email", async (req, res) => {
+      const { email } = req.params;
+
+      console.log(email);
+
+      const borrowedBookList = await borrowedBooksCollection
+        .find({ email })
+        .toArray();
+
+      console.log(borrowedBookList);
+
+      res.send(borrowedBookList);
+    });
+
+    app.get("/book/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = await booksCollection.findOne({ _id: new ObjectId(id) });
+      res.send(data);
+    });
+
+    app.get("/books", async (req, res) => {
+      const data = await booksCollection.find({}).toArray();
+      res.send(data);
+    });
+
     app.get("/books/:category", async (req, res) => {
       const { category } = req.params;
       const data = await booksCollection.find({ category }).toArray();
       res.send(data);
     });
-
-    
 
     app.get("/books/details/:id", async (req, res) => {
       const { id } = req.params;
